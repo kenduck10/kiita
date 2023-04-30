8b8cd118fb80b239d00123f6035b28979606f935
kentaro_kami <kentaro_kami@kenduck.com>
2023-04-30 12:05:25 +0900

    SSRページ追加

--- frontend/consumer/src/pages/index.tsx
+++ frontend/consumer/src/pages/index.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
+type User = {
+  userId: number;
+  lastName: string;
+  firstName: string;
+};
+export default ({ users }: { users: Array<User> }) => {
+  console.log(users);
   return <h1>home</h1>;
-}
+};
+
+export const getServerSideProps = async () => {
+  const response = await (await fetch(`${process.env.KIITA_API_BASE_URL}users`)).json();
+  return {
+    props: {
+      users: response.users,
+    },
+  };
+};
