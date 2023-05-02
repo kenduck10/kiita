5cc1b23d953cfde31af48ca370465d26bcfcd9a1
kentaro_kami <kentaro_kami@kenduck.com>
2023-05-02 10:06:18 +0900

    アラート追加

--- frontend/consumer/src/pages/users/new/index.tsx
+++ frontend/consumer/src/pages/users/new/index.tsx
@@ -1,10 +1,12 @@
 import * as yup from 'yup';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import { Button } from '@mui/material';
+import { Alert, Button } from '@mui/material';
 import { ControlledTextField } from '@/components/elements/ControlledTextField';
 import { FIRST_NAME_YUP_SCHEMA, LAST_NAME_YUP_SCHEMA } from '@/features/user/validations/YupSchema';
 import axios, { HttpStatusCode } from 'axios';
 import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 type SubmitArguments = {
   lastName: string;
@@ -18,6 +20,7 @@ const errorSchema = yup.object().shape({
 
 export const NewUser = () => {
   const router = useRouter();
+  const [submitErrorMessage, setSubmitErrorMessage] = useState('');
 
   const { control, handleSubmit } = useForm<SubmitArguments>({
     mode: 'all',
@@ -27,16 +30,16 @@ export const NewUser = () => {
       lastName: '',
       firstName: '',
     },
-    // resolver: yupResolver(errorSchema),
+    resolver: yupResolver(errorSchema),
   });
   const onSubmit: SubmitHandler<SubmitArguments> = async (data) => {
+    setSubmitErrorMessage('');
     await axios
       .post(`${process.env.NEXT_PUBLIC_KIITA_FRONTEND_API_BASE_URL}users`, data)
       .then(() => router.push('/'))
       .catch((error) => {
-        console.log(error.response.status);
         if (error.response.status === HttpStatusCode.BadRequest) {
-          console.log('入力内容に誤りがあります');
+          setSubmitErrorMessage('入力内容に誤りがあります');
           return;
         }
         router.push('/error');
@@ -45,6 +48,7 @@ export const NewUser = () => {
 
   return (
     <div>
+      {submitErrorMessage && <Alert severity="error">{submitErrorMessage}</Alert>}
       <ControlledTextField control={control} name={'lastName'} type={'text'} label={'姓'} />
       <ControlledTextField control={control} name={'firstName'} type={'text'} label={'名'} />
       <Button variant="contained" onClick={handleSubmit(onSubmit)}>
