899e7a52dd0ce128c28880a2dc76a9357d8ac648
kentaro_kami <kentaro_kami@kenduck.com>
2023-05-08 12:23:11 +0900

    ヘッダデザイン追加

--- frontend/consumer/src/components/layouts/Layout.tsx
+++ frontend/consumer/src/components/layouts/Layout.tsx
@@ -1,12 +1,15 @@
 import Header from '@/components/layouts/Header/Header';
 import Footer from '@/components/layouts/Footer/Footer';
 import { ReactElement } from 'react';
+import { Container } from '@mui/material';
 
 export const Layout = ({ children }: { children: ReactElement }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
+      <Container maxWidth="xl">
+        <main>{children}</main>
+      </Container>
       <Footer />
     </>
   );
