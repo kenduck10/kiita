de5b08f7989639745d097456a7c7ddf6c5c8c9ab
kenduck10 <132117943+kenduck10@users.noreply.github.com>
2023-05-08 12:23:43 +0900

    ヘッダデザイン追加 (#15)

--- frontend/consumer/src/components/layouts/Header/Header.tsx
+++ frontend/consumer/src/components/layouts/Header/Header.tsx
@@ -1,9 +1,19 @@
+import { AppBar, Container, Toolbar } from '@mui/material';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export const Header = () => {
   return (
     <header>
-      <h1>
-        <a href={'/'}>Kiita</a>
-      </h1>
+      <AppBar>
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Link href={'/'}>
+              <Image src="logo.svg" alt="logo" width="96" height="32" />
+            </Link>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </header>
   );
 };
