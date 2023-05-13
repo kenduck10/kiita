451cea09573042947b0508815ba2a2d070a57959
kentaro_kami <kentaro_kami@kenduck.com>
2023-05-13 15:21:54 +0900

    不要ファイル削除

--- frontend/consumer/src/hooks/useLoad.ts
+++
@@ -1,8 +0,0 @@
-import { useRecoilState } from 'recoil';
-import { isLoadingState } from '@/stores/load';
-
-export const useLoad = () => {
-  const [isLoading, setIsLoading] = useRecoilState(isLoadingState);
-  const startLoad = () => setIsLoading(true);
-  return { isLoading, startLoad };
-};
--- frontend/consumer/src/stores/submit.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from 'recoil';
-
-export const isSubmittingState = atom({
-  key: 'isSubmitting',
-  default: false,
-});
