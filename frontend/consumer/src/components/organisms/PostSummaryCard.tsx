d425b189778c756d094d475ce98b78f550c6a2fd
kentaro_kami <kentaro_kami@kenduck.com>
2023-05-30 10:45:20 +0900

    詳細画面リンク追加

--- frontend/consumer/src/components/organisms/PostSummaryCard.tsx
+++ frontend/consumer/src/components/organisms/PostSummaryCard.tsx
@@ -1,7 +1,9 @@
-import { Card, CardContent, SxProps, Theme, Typography } from '@mui/material';
+import { Card, CardActionArea, CardContent, SxProps, Theme, Typography } from '@mui/material';
 import React from 'react';
 import PostSummary from '@/features/post/models/PostSummary';
 import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+import { PAGE_PATH_BUILDER } from '@/utils/consts/route';
 
 const StyledCardContent = styled(CardContent)({
   [`&:last-child`]: {
@@ -10,13 +12,19 @@ const StyledCardContent = styled(CardContent)({
 });
 
 export const PostSummaryCard = ({ postSummary, sx }: { postSummary: PostSummary; sx?: SxProps<Theme> }) => {
+  const router = useRouter();
+  const onClickCard = async () => {
+    await router.push(PAGE_PATH_BUILDER.POST_DETAIL(postSummary.id));
+  };
   const baseSx = { border: 0 };
   const mergedSx = sx ? { ...baseSx, ...sx } : baseSx;
   return (
     <Card variant={'outlined'} sx={mergedSx}>
-      <StyledCardContent>
-        <Typography>{postSummary.title}</Typography>
-      </StyledCardContent>
+      <CardActionArea onClick={onClickCard}>
+        <StyledCardContent>
+          <Typography>{postSummary.title}</Typography>
+        </StyledCardContent>
+      </CardActionArea>
     </Card>
   );
 };
