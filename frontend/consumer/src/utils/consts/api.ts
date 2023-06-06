bcf8992a3d00dba891cc01c5013849e55a166374
kentaro_kami <kentaro_kami@kenduck.com>
2023-06-06 14:48:03 +0900

    コメント削除フック追加

--- frontend/consumer/src/utils/consts/api.ts
+++ frontend/consumer/src/utils/consts/api.ts
@@ -7,6 +7,7 @@ const API_SUB_PATH_BUILDER = {
   USER: (userId: number) => `/users/${userId}`,
   POST: (postId: number) => `/posts/${postId}`,
   POST_COMMENTS: (postId: number) => `/posts/${postId}/comments`,
+  COMMENT: (commentId: number) => `/comments/${commentId}`,
 } as const;
 
 const FRONTEND_BASE_PATH = process.env.NEXT_PUBLIC_KIITA_FRONTEND_API_BASE_URL;
@@ -20,6 +21,7 @@ export const FRONTEND_API_PATH_BUILDER = {
   USER: (userId: number) => FRONTEND_BASE_PATH + API_SUB_PATH_BUILDER.USER(userId),
   POST: (postId: number) => FRONTEND_BASE_PATH + API_SUB_PATH_BUILDER.POST(postId),
   POST_COMMENTS: (postId: number) => FRONTEND_BASE_PATH + API_SUB_PATH_BUILDER.POST_COMMENTS(postId),
+  COMMENT: (commentId: number) => FRONTEND_BASE_PATH + API_SUB_PATH_BUILDER.COMMENT(commentId),
 } as const;
 
 const BACKEND_BASE_PATH = process.env.KIITA_BACKEND_API_BASE_URL;
@@ -32,4 +34,5 @@ export const BACKEND_API_PATH_BUILDER = {
   USER: (userId: number) => BACKEND_BASE_PATH + API_SUB_PATH_BUILDER.USER(userId),
   POST: (postId: number) => BACKEND_BASE_PATH + API_SUB_PATH_BUILDER.POST(postId),
   POST_COMMENTS: (postId: number) => BACKEND_BASE_PATH + API_SUB_PATH_BUILDER.POST_COMMENTS(postId),
+  COMMENT: (commentId: number) => BACKEND_BASE_PATH + API_SUB_PATH_BUILDER.COMMENT(commentId),
 } as const;
