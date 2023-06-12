41584245862225eb8e11ef32cd15b77b09da2053
kentaro_kami <kentaro_kami@kenduck.com>
2023-06-13 07:42:02 +0900

    ユニーク制約追加

--- backend/common/migration/common/V00.07.00__create_members.sql
+++ backend/common/migration/common/V00.07.00__create_members.sql
@@ -1,7 +1,7 @@
 CREATE TABLE members
 (
     id           INT AUTO_INCREMENT                                              NOT NULL PRIMARY KEY,
-    name         VARCHAR(50)                                                     NOT NULL,
+    name         VARCHAR(50)                                                     NOT NULL UNIQUE,
     mail_address VARCHAR(319)                                                    NOT NULL UNIQUE,
     created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP                             NOT NULL,
     updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL
