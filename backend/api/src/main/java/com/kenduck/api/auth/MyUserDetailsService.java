bee62afed3f3bf30080178fabf729ce680a8e219
kentaro_kami <kentaro_kami@kenduck.com>
2023-06-19 13:57:27 +0900

    コメント追加

--- backend/api/src/main/java/com/kenduck/api/auth/MyUserDetailsService.java
+++ backend/api/src/main/java/com/kenduck/api/auth/MyUserDetailsService.java
@@ -12,6 +12,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+/**
+ * DB利用した認証処理を受け持つクラス
+ */
 @Service
 @RequiredArgsConstructor
 public class MyUserDetailsService implements UserDetailsService {
@@ -22,6 +25,14 @@ public class MyUserDetailsService implements UserDetailsService {
     @NonNull
     private final MemberPasswordMapper memberPasswordMapper;
 
+    /**
+     * ユーザー名から認証処理実施
+     * 具体的なユーザー情報を返す
+     *
+     * @param name ユーザー名
+     * @return ユーザー詳細 {@link MyUserDetails}
+     * @throws UsernameNotFoundException ユーザー名NotFound例外
+     */
     @Override
     public UserDetails loadUserByUsername(String name) throws UsernameNotFoundException {
         Member member = memberMapper.selectByName(name)
