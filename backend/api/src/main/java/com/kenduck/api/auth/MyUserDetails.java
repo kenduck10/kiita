decab28757a42e3f13810e61db73e27fdf60b489
kentaro_kami <kentaro_kami@kenduck.com>
2023-06-19 13:50:13 +0900

    MyUserDetails整理

--- backend/api/src/main/java/com/kenduck/api/auth/MyUserDetails.java
+++ backend/api/src/main/java/com/kenduck/api/auth/MyUserDetails.java
@@ -7,11 +7,21 @@ import org.springframework.security.core.userdetails.User;
 
 import java.util.Collections;
 
+/**
+ * 認証後のユーザー情報
+ */
 @Getter
 public class MyUserDetails extends User {
 
     private final Member member;
 
+    /**
+     * コンストラクタ
+     * パスワードは暗号化されたものを保持
+     *
+     * @param member         {@link Member}
+     * @param memberPassword {@link MemberPassword}
+     */
     public MyUserDetails(Member member, MemberPassword memberPassword) {
         super(member.getName(), memberPassword.getPassword(), Collections.emptyList());
         this.member = member;
