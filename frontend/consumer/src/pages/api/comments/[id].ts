5ddb551c3a832441da83fd4e47a769261cabe89a
kentaro_kami <kentaro_kami@kenduck.com>
2023-06-25 21:32:21 +0900

    リクエストにトークン詰めるようにした

--- frontend/consumer/src/pages/api/comments/[id].ts
+++ frontend/consumer/src/pages/api/comments/[id].ts
@@ -1,13 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { BACKEND_API_PATH_BUILDER } from '@/utils/consts/api';
-import { requestDelete } from '@/utils/functions/api';
+import { buildXAuthTokenHeader, getAccessToken, requestDelete } from '@/utils/functions/api';
+import { HttpStatusCode } from 'axios';
 
 export const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   const { id } = request.query;
   const apiPath = BACKEND_API_PATH_BUILDER.COMMENT(Number(id));
 
+  const accessToken = await getAccessToken(request, response);
+  if (!accessToken) {
+    return response.status(HttpStatusCode.Unauthorized);
+  }
+
+  const config = { headers: buildXAuthTokenHeader(accessToken) };
+
   if (request.method === 'DELETE') {
-    return requestDelete(apiPath, response);
+    return requestDelete(apiPath, response, config);
   }
   return response.status(400);
 };
